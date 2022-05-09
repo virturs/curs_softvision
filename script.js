8ce20d851715c1879294e7f163b66466b427e1a8
Mihai Cruceanu <cybervirus_98@yahoo.com>
2022-05-09 16:48:59 +0300

    Add files via upload
    @vanceacorina97 tema 2

---
+++ script.js
@@ -0,0 +1,20 @@
+//Creați o funcție care primește ca și parametru un număr și returnează dublul său dacă numărul primit este impar, altfel returnează numărul scăzut cu 2 dacă este par. Afișați rezultatul în consolă.
+
+/*Creați o funcție care stochează intr-un array doar numerele prime dintr-un array primit ca si parametru. Parcurgeți array-ul folosind un “for loop”. Afișați rezultatul în consola.
+Ex:
+const arr = [1,2,3,4,5,6,7,8,9];
+savePrimes(arr) => [1,2,3,5,7];*/
+
+/*Creați o funcție care stochează intr-un array doar strings dintr-un array primit ca si parametru. Parcurgeți array-ul folosind “forEach”. Afișați rezultatul în consola.
+Ex:
+saveStrings([1, ‘1’, ‘salut’, 2, 6, true, ‘true’]) -> [‘1’, ‘salut’, ‘true’]
+saveStrings([1,2,3,4]) -> []
+saveStrings([‘1’,’2’,’3’,’4’]) -> [‘1’,’2’,’3’,’4’]*/
+
+/*Creați o funcție care adună cifrele impare ale unui număr primit ca și parametru de tip string folosind  “for loop” si keyword-ul “continue”. 
+Ex: addDigits(“1523”) => 9*/
+
+/*Creați un switch cu cel puțin 5 case-uri din care cel puțin 2 să aibă același rezultat, după exemplul prezentat la curs.
+Folosiți cel puțin 2 tipuri la alegere de creare a funcțiilor în rezolvarea exercițiilor ( Exemplu, exercițiul 1 și 2 cu arrow function, exercițiul 3 și 4 cu function statement - în curs au fost prezentate 4 metode de a crea funcții) 
+Repository-ul cu codul de la cursul 2 https://github.com/vanceacorina97/CursSV2022/tree/main/Curs%202 
+Înregistrarea video de la cursul 2: https://www.youtube.com/watch?v=fLtLhbDPkoI*/
